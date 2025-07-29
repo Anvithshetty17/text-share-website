a8703544c8d46d0b583e7ec346dd57776dcf3b2c
Anvith shetty <161817374+Anvithshetty17@users.noreply.github.com>
2025-07-29 20:04:17 +0530

    Add files via upload

---
+++ gotext-prep/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CodingPage from './pages/CodingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <CodingPage />
+    </div>
+  );
+}
+
+export default App;
