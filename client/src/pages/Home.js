6f7f58285f5292c3d680eb99b2ea83e24a698dd4
Anvith shetty <161817374+Anvithshetty17@users.noreply.github.com>
2025-08-04 08:29:42 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AdBanner from "../components/AdBanner";
 import BannerAdBox from "../components/BannerAdBox";
+import { useEffect } from "react";
 
 
 
